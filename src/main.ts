c78fdb5cc9de13694d8acd011afde73ab803a3a7
Albert <ksk836684@gmail.com>
2024-10-27 02:57:53 +0400

    feat add pinia

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { createApp } from 'vue'
 import {router} from "./pages/router.ts"
+import { createPinia } from 'pinia'
 import './styles/global.scss'
 import App from './App.vue'
 
+const pinia = createPinia()
+
 createApp(App)
     .use(router)
+    .use(pinia)
     .mount('#app')
