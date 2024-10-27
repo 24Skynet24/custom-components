7619270b8bf1fc283bad6ae6c5a5d41761e49e77
Albert <ksk836684@gmail.com>
2024-10-27 16:26:32 +0400

    feat add header

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 
+import HeaderCmp from "@components/layouts/HeaderCmp.vue"
 </script>
 
 <template>
   <div>
+    <header-cmp/>
     <router-view/>
   </div>
 </template>
---
+++ src/components/layouts/HeaderCmp.vue
@@ -0,0 +1,57 @@
+<script lang="ts" setup>
+import LogoLink from "@/ui/links/LogoLink.vue"
+
+const navRoutes = [
+  {
+    url: '/',
+    name: 'Home'
+  },
+  {
+    url: '/components',
+    name: 'Components'
+  }
+]
+</script>
+
+<template>
+  <header class="header" role="banner">
+    <div class="container flex items-center justify-between">
+      <logo-link/>
+
+      <nav role="navigation">
+        <ul class="flex items-center gap-8" role="list">
+          <li v-for="link in navRoutes" :key="`header_link_${link.name}`">
+            <router-link :to="link.url" role="link">
+              {{ link.name }}
+            </router-link>
+          </li>
+        </ul>
+      </nav>
+
+      <!-- Refactor search -->
+      <input type="search" placeholder="Search" class="test-search" role="searchbox">
+    </div>
+  </header>
+</template>
+
+<style scoped lang="scss">
+
+header {
+  padding: 1rem 0;
+
+  nav {
+    a {
+      font-size: 1.8rem;
+    }
+  }
+
+  .test-search {
+    width: 20rem;
+    padding: 1rem;
+    font-size: 1.6rem;
+    border: 1px solid var(--black);
+    border-radius: 8px;
+  }
+}
+
+</style>
