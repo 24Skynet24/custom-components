d225e8cb78d01d693a28c43bfc9a252abc7e1003
Albert <ksk836684@gmail.com>
2024-10-27 23:24:35 +0400

    feat add hello section

---
+++ src/components/sections/HelloSection.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <section class="flex-center">
+    <div class="section__inner flex-center flex-col">
+      <h1 class="text-center">Hello world!</h1>
+      <p class="text-center">
+        Здесь вы найдете множество кастомных и функциональных компонентов,
+        которые помогут вам в разработке на Vue и React.
+        Упростите свою работу и создавайте замечательные приложения с нашими готовыми решениями!
+      </p>
+    </div>
+  </section>
+</template>
+
+<style scoped lang="scss">
+.section__inner {
+  width: 75rem;
+  padding-top: 4rem;
+
+  h1 {
+    font-size: 4.8rem;
+    margin-bottom: 2rem;
+  }
+
+  p {
+    font-size: 1.8rem;
+    line-height: 170%;
+  }
+}
+</style>
