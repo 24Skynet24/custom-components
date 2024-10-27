60fbdb0888b2efa2e7eb3225112bd8e0b06389cd
Albert <ksk836684@gmail.com>
2024-10-27 23:24:17 +0400

    feat add Card for components

---
+++ src/components/cards/ComponentCard.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+import { Card } from "ant-design-vue"
+import {ComponentCardType} from "@/types/index.ts"
+import {defineProps} from "vue";
+
+defineProps<{
+  cardInfo: ComponentCardType,
+}>()
+
+</script>
+
+<template>
+  <Card :title="cardInfo.title">
+    <template #extra>
+      <router-link :to="cardInfo.link.url">
+        {{ cardInfo.link.name }}
+      </router-link>
+    </template>
+    <img :src="cardInfo.img" alt="">
+  </Card>
+</template>
