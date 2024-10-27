a0a4185dcec61f573d29877067df81a193e0cf6c
Albert <ksk836684@gmail.com>
2024-10-27 23:24:27 +0400

    feat add custom section

---
+++ src/components/sections/CustomSection.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import {defineProps} from "vue"
+
+defineProps({
+  sectionTitle: String,
+})
+</script>
+
+<template>
+  <section class="section">
+    <h2 class="section__title">{{ sectionTitle }}</h2>
+    <div class="section__content">
+      <slot/>
+    </div>
+  </section>
+</template>
+
+
+<style scoped lang="scss">
+.section__title {
+    font-size: 2rem;
+    margin-bottom: 3rem;
+}
+</style>
